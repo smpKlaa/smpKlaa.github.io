bc56e842487687d9df1a55d8be5f61391e54afee
smpKlaa <96190615+smpKlaa@users.noreply.github.com>
2022-08-23 11:12:20 +0300

    Korjaan vanhat tehtävät

--- koulu/vuosi2/harjoituksia_A/harjoitus_A/Program.cs
+++ koulu/vuosi2/harjoituksia_A/harjoitus_A/Program.cs
@@ -10,8 +10,37 @@ namespace harjoitus_A
             Console.WriteLine("--------------------");
             Console.WriteLine();
 
-            //Tulostetaan tervehdys
+            //3.Tehtävä
+            //
+            //Ohjelma tulostaa tervehdyksen.
             Console.WriteLine("Hyvää päivää!");
+
+
+
+            //4.Tehtävä
+            //
+            //Ohjelma tulostaa etu- ja sukinimeni.
+            Console.WriteLine("Sampo Klaavo");
+
+
+
+            //5.Tehtävä
+            //
+            //Ohjelma tulostaa sarjan numeroita eri riveille.
+            Console.WriteLine("1\n11\n101\n1001");
+
+
+
+            //6.Tehtävä
+            //
+            //Ohjelma kysyy käyttäjän nimeä ja tulostaa tervehdyksen käyttäen käyttäjän nimeä.
+
+            //Käyttäjän nimi kysytään. Nimi tallennetaan muuttujaan.
+            Console.WriteLine("Kirjoita nimesi...");
+            string nimi = Console.ReadLine();
+
+            //Tulostetaan tervehdys käyttäen käyttäjän nimeä.
+            Console.WriteLine("Hei " + nimi);
         }
     }
 }
