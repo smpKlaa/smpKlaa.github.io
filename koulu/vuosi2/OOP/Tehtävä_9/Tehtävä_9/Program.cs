a3446ad4c943148df10a9f5c6b472ab632367992
smpKlaa <96190615+smpKlaa@users.noreply.github.com>
2022-10-03 12:54:33 +0300

    Oliotehtävä 9 pohja

---
+++ koulu/vuosi2/OOP/Tehtävä_9/Tehtävä_9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tehtävä_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
